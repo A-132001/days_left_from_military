a3a66d2f68db1028a7898f1c2297c9cd110dc8a0
Abdelrahman Sabry <75238226+A-132001@users.noreply.github.com>
2024-01-07 09:33:47 +0200

    Update script.js
    add time left the day

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ var daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 // Display the countdown
 var countdownElement = document.getElementById("countdown");
 countdownElement.innerHTML = "Days Left " + daysLeft;
+
+// add time left the the day
+function displayTime() {
+  var currentDate = new Date();
+  var hours = 24 - currentDate.getHours();
+  var minutes = 60 - currentDate.getMinutes();
+  var seconds = 60 - currentDate.getSeconds();
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  var timeString = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("clock").innerHTML = timeString;
+}
+setInterval(displayTime, 1000); // update every second
